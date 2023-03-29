586f9f010e0d7ab0a34c25f3ca596277e0bdd949
CBIS2022Cosmepro <123550581+CBIS2022Cosmepro@users.noreply.github.com>
2023-03-29 11:56:10 +0200

    more cmds

--- main.ts
+++ main.ts
@@ -1,9 +1,13 @@
 input.onButtonPressed(Button.A, function () {
     HAPPY.showImage(0)
 })
+input.onButtonPressed(Button.AB, function () {
+    normal.showImage(0)
+})
 input.onButtonPressed(Button.B, function () {
     Sad.showImage(0)
 })
+let normal: Image = null
 let Sad: Image = null
 let HAPPY: Image = null
 HAPPY = images.createImage(`
@@ -19,6 +23,13 @@ Sad = images.createImage(`
     . . # . .
     . # . # .
     # . . . #
+    `)
+normal = images.createImage(`
+    . # . # .
+    . . . . .
+    . . . . .
+    # # # # #
+    . . . . .
     `)
 basic.forever(function () {
 	
