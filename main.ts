feef24d87a90890dd7a6cf51289e0dabbb8d6672
CBIS2022Cosmepro <123550581+CBIS2022Cosmepro@users.noreply.github.com>
2023-03-29 11:38:03 +0200

    new codes

--- main.ts
+++ main.ts
@@ -1,3 +1,25 @@
+input.onButtonPressed(Button.A, function () {
+    HAPPY.showImage(0)
+})
+input.onButtonPressed(Button.B, function () {
+    Sad.showImage(0)
+})
+let Sad: Image = null
+let HAPPY: Image = null
+HAPPY = images.createImage(`
+    . # . # .
+    . . . . .
+    # . . . #
+    . # . # .
+    . . # . .
+    `)
+Sad = images.createImage(`
+    . # . # .
+    . . . . .
+    . . # . .
+    . # . # .
+    # . . . #
+    `)
 basic.forever(function () {
 	
 })
