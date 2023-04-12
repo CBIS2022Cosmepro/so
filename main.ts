14e91ed25b9e5cdf51564dbf74923652ec7e3b5d
CBIS2022Cosmepro <123550581+CBIS2022Cosmepro@users.noreply.github.com>
2023-04-12 11:56:13 +0200

    commands

--- main.ts
+++ main.ts
@@ -7,6 +7,12 @@ input.onButtonPressed(Button.AB, function () {
 input.onButtonPressed(Button.B, function () {
     Sad.showImage(0)
 })
+input.onGesture(Gesture.Shake, function () {
+    Sad.showImage(0)
+    _.showImage(0)
+    music.playSoundEffect(music.createSoundEffect(WaveShape.Sine, 4764, 1695, 255, 255, 465, SoundExpressionEffect.None, InterpolationCurve.Logarithmic), SoundExpressionPlayMode.UntilDone)
+})
+let _: Image = null
 let normal: Image = null
 let Sad: Image = null
 let HAPPY: Image = null
@@ -31,6 +37,19 @@ normal = images.createImage(`
     # # # # #
     . . . . .
     `)
+_ = images.createImage(`
+    # . . . #
+    . # . # .
+    . . # . .
+    . # . # .
+    # . . . #
+    `)
 basic.forever(function () {
 	
+})
+basic.forever(function () {
+    if (input.logoIsPressed()) {
+        basic.showIcon(IconNames.Heart)
+        basic.showIcon(IconNames.Happy)
+    }
 })
